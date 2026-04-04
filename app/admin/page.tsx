74d5d45f81afe48eb52f00d45b9929229fcf89df
edgar <lianloga11@gmail.com>
2026-04-04 15:59:28 +0300

    Improve mobile grid layout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { supabase } from './supabase'
-
+import { supabase } from '../supabase'
 type Product = {
   id: number
   name: string
